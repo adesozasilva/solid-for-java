0f8d6c52c9aea5fb8355b41d22ffe1bb57f484b3
Anderson Silva <adesozasilva@gmail.com>
2019-08-05 07:35:45 -0300

    First commit

---
+++ src/main/java/br/com/adesozasilva/solidforjavaliskov/SolidForJavaLiskovApplication.java
@@ -0,0 +1,13 @@
+package br.com.adesozasilva.solidforjavaliskov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidForJavaLiskovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidForJavaLiskovApplication.class, args);
+	}
+
+}
